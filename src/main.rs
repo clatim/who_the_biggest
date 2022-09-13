7ef8424a20e899563cc834c665342f2c3b2f3c0a
charlie <clatimer159@gmail.com>
2022-09-13 22:13:30 +0100

    Moved matrix generation into function.
    This function returns the maximum weight. It needs to be added
    so that it returns the position of it too.

--- src/main.rs
+++ src/main.rs
@@ -1,35 +1,41 @@
 use rand::Rng;
-use nalgebra::{DMatrix};
+use nalgebra::{DMatrix,DVector};
 
 fn main() {
     println!("Hello, world!");
 
-    let num_unknowns: usize = 3;
+    let difficulty = 2;
+    let num_unknowns: usize = difficulty;
 
+    let answer: f64 = generate_solution(num_unknowns);
+    // TODO let user guess the position based on the system.
+}
+
+fn generate_solution(num_unknowns: usize) -> f64 {
     let mut rng = rand::thread_rng();
     // Generate a matrix
     let a_coefficients: Vec<f64> = (0..num_unknowns.pow(2)).map(|_| rng.gen_range(0.0..10.0)).collect();
-    println!("Random vector {a_coefficients:?}");
     let m1 = DMatrix::from_vec(num_unknowns, num_unknowns, a_coefficients);
-    println!("m1 = {}", m1);
 
     // Now generate a RHS
     let b_coeffs: Vec<f64> = (0..num_unknowns).map(|_| rng.gen_range(0.0..10.0)).collect();
     let b = DMatrix::from_vec(num_unknowns, 1, b_coeffs);
-    println!("RHS will be {}", b);
+    // println!("RHS will be {}", b);
+    println!("The system is {m1} = {b}");
 
     // Now invert the matrix to find the weights
-    let weights: DMatrix<f64>;
+    let max_weight: f64;
     match m1.try_inverse() {
         Some(inv) => {
-            println!("The inverse of m1 is: {}", inv);
-            weights = inv * b;
+            let weights: DMatrix<f64> = inv * b;
             println!("The weights are {}", weights);
+            max_weight = weights.max();            
         }
         None => {
             println!("m1 is not invertible!");
+            max_weight = 0.0;
         }
     }
-    
-    
+    // TODO return position of maximum weight
+    return max_weight;
 }
