700b3138a270371a361cf0340af5dbfdc7f0c67b
charlie <clatimer159@gmail.com>
2022-09-15 11:39:52 +0100

    Now returns max value and its index.
    Also made the RHS a vector instead of a matrix of size `(1, num_unknowns)`.

--- src/main.rs
+++ src/main.rs
@@ -7,20 +7,17 @@ enum InversionError {
 }
 
 fn main() {
-    println!("Hello, world!");
-
     let difficulty = 2;
     let num_unknowns: usize = difficulty;
 
-    // let answer: f64 = generate_solution(num_unknowns);
     match generate_solution(num_unknowns) {
-        Ok(weight) => println!("Max weight is {weight}"),
+        Ok(weight) => println!("Max weight and index is {weight:?}"),
         Err(_) => println!("Matrix was singular. Try again!"),
     }
     // TODO let user guess the position based on the system.
 }
 
-fn generate_solution(num_unknowns: usize) -> Result<f64, InversionError> {
+fn generate_solution(num_unknowns: usize) -> Result<(usize,f64), InversionError> {
     let mut rng = rand::thread_rng();
     // Generate a matrix
     let a_coefficients: Vec<f64> = (0..num_unknowns.pow(2)).map(|_| rng.gen_range(0.0..10.0)).collect();
@@ -28,18 +25,17 @@ fn generate_solution(num_unknowns: usize) -> Result<f64, InversionError> {
 
     // Now generate a RHS
     let b_coeffs: Vec<f64> = (0..num_unknowns).map(|_| rng.gen_range(0.0..10.0)).collect();
-    let b = DMatrix::from_vec(num_unknowns, 1, b_coeffs);
+    let b = DVector::from_vec(b_coeffs);
     // println!("RHS will be {}", b);
     println!("The system is {m1} = {b}");
 
     // Now invert the matrix to find the weights
-    let max_weight: f64;
     match m1.try_inverse() {
         Some(inv) => {
-            let weights: DMatrix<f64> = inv * b;
+            let weights = inv * b;
             println!("The weights are {}", weights);
-            // TODO return position of maximum weight
-            Ok(weights.max())
+            println!("the positon of max is {:?}", weights.argmax());
+            Ok(weights.argmax())
         }
         None => {
             Err(InversionError::SingularMatrix)
